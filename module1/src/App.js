ea4fa831c6c926000e75b359f3276df465e86aba
francois <francois.vandeputte@student.vinci.be>
2023-09-15 10:33:01 +0200

    module1 fin

--- module1/src/App.js
+++ module1/src/App.js
@@ -3,23 +3,40 @@ const Hello = (props) => {
 }
 
 const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-  console.log(now, a+b)
+    const now = new Date()
+    const a = 10
+    const b = 20
+    const friends = [
+        { name: 'Dami1', age: 12 },
+        { name: 'Migouel', age: 3 },
+        { name: 'Kyle', age: 14 },
+        { name: 'KekMan', age: 99999 },
+        { name: 'Peter', age: 4 }
+    ]
 
-  return (
-      <div>
-        <p>Hello world, it is {now.toString()}</p>
-        <p>
-            {a} + {b} = {a + b}
-        </p>
-          <Hello name = "Dami1" age = "12"/>
-          <Hello name = "Migouel" age = "3"/>
-          <Hello name = "Kyle" age = "14"/>
-          <Hello name = "KekMan" age = "99999"/>
-      </div>
-  )
+    return (
+        <div>
+            <p>Hello world, it is {now.toString()}</p>
+            <p>
+                {a} + {b} = {a + b}
+            </p>
+            <Hello name={friends[0].name} age={friends[0].age}/>
+            <Hello name={friends[1].name} age={friends[1].age}/>
+            <Hello name={friends[2].name} age={friends[2].age}/>
+            <Hello name={friends[3].name} age={friends[3].age}/>
+            <Hello name={friends[4].name} age={friends[4].age}/>
+
+            <Footer />
+        </div>
+    )
+}
+
+const Footer = () => {
+    return (
+        <div>
+            greeting app created by <a href='https://github.com/mluukkai'>mluukkai</a>
+        </div>
+    )
 }
 
 export default App;
