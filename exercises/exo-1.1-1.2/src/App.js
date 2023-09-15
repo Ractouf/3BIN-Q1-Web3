4b68b134625d38991321739dd9f2b38a1cc1b405
francois <francois.vandeputte@student.vinci.be>
2023-09-15 11:43:50 +0200

    1.1-1.2 : refactor - absolute imports

--- exercises/exo-1.1-1.2/src/App.js
+++ exercises/exo-1.1-1.2/src/App.js
@@ -1,6 +1,6 @@
-import Header from "./components/Header";
-import Content from "./components/Content";
-import Total from "./components/Total";
+import Header from "components/Header";
+import Content from "components/Content";
+import Total from "components/Total";
 
 const App = () => {
     const course = 'Half Stack application development';
